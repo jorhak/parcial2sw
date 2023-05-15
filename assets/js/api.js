5f42e98df931731865350daabb6177666d164bc7
Pierluigi Vaca Garcia <itfuturovaca@gmail.com>
2023-05-15 16:22:59 -0400

    Envia paquete,nombre,frase

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,35 +8,35 @@ export const listUsers = async () => {
         let content = ``;
         datos.forEach((dato) => {
             content += `
-	             <div class="modal fade" id="staticBackdrop${dato.id}" data-bs-backdrop="static" data-bs-keyboard="false"
+	             <div class="modal fade" id="modelo${dato.id}" data-bs-backdrop="static" data-bs-keyboard="false"
                     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                         <div class="modal-content text-bg-dark">
                             <div class="modal-header">
                                 <h1 class="modal-title fs-5" id="staticBackdropLabel">${dato.origen} - ${dato.destino} (${dato.turno})</h1>
-				<button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
+				                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <p>Origen: ${dato.origen}</p>
                                 <span>Destino: ${dato.destino}</span>
                                 <p>Costo: ${dato.costo}</p>
                                 <span>Dias: ${dato.duracion} </span>
-				<form>
-          			   <div class="form-group">
-            			      <input type="text" class="form-control" value="${dato.id}" style="display:none" id="paquete">
-				      <label for="nombre">Nombre:</label>
-            			      <input type="text" class="form-control" id="nombre">
-          			   </div>
-          			   <div class="form-group">
-            			      <label for="comentario">Comentario:</label>
-            			      <textarea class="form-control" rows="5" id="comentario"></textarea>
-          			   </div>
-        			</form>
+                                <form id="formulario-comentario">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="paquete" value="${dato.id}" style="display:none">
+                                        <label for="nombre">Nombre:</label>
+                                        <input type="text" class="form-control" id="nombre">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="comentario">Comentario:</label>
+                                        <textarea class="form-control" rows="5" id="comentario"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </form>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <div type="submit" id="sendMessage">Aceptar</div>
+                                <button type="button" class="btn btn-primary" id="guardar">Guardar</button>
                             </div>
                         </div>
                     </div>
@@ -49,7 +49,7 @@ export const listUsers = async () => {
                             <p class="card-text">${dato.descripcion}</p>
                                 <!-- Button trigger modal -->
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#staticBackdrop${dato.id}">
+                                data-bs-target="#modelo${dato.id}">
                                 Más información
                              </button>
                         </div>
@@ -64,7 +64,7 @@ export const listUsers = async () => {
     }
 };
 
-listUsers()
+//listUsers()
 
 const inputVozElement = document.querySelector("#input-voz")
 const recognition = new webkitSpeechRecognition();
@@ -85,7 +85,7 @@ recognition.onresult = (event) => {
     inputVozElement.value = texto;
 }
 
-function leerTexto(text){
+function leerTexto(text) {
     const speech = new SpeechSynthesisUtterance(text);
     speech.volume = 1;
     speech.rate = 1;
--- assets/js/comentario.js
+++ assets/js/comentario.js
@@ -1,35 +1,43 @@
-import  { listUsers } from './api.js'
+import { listUsers } from './api.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+  // Esperar a que se carguen los datos
+  listUsers().then(() => {
+    // Capturar el formulario de comentarios
+    const formularioComentario = document.getElementById('formulario-comentario');
+    // Agregar un evento para el envío del formulario
+    formularioComentario.addEventListener('submit', async event => {
+      event.preventDefault();
 
+      // Obtener los valores del formulario
+      const paquete = document.getElementById('paquete').value
+      const nombre = document.getElementById('nombre').value;
+      const comentario = document.getElementById('comentario').value;
 
+      console.log(`Nombre: ${nombre} Comentario: ${comentario}`)
 
+      // Enviar los valores por POST a la API
+      await fetch('http://localhost:3000/api/review', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "id_paquete": paquete,
+          "nombre_cliente": nombre,
+          "frase": comentario
+        })
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    });
+  });
 
-async function main(){
-const modal = await listUsers()
+  
 
-   const sendElemento = document.getElementById('sendMessage') 
-   async function sendMessage(){
-      const paquete = document.getElementById('paquete')
-      const nombre = document.getElementById('nombre')
-      const comentario = document.getElementById('comentario')
-
-      const options={
-         method: "POST",
-         headers: {"Content-Type": "application/json"},
-         body: JSON.stringify({
-	   messages:[{paquete: paquete.value, nombre:nombre.value , comentario:comentario.value}]
-         }),
-      }
-      try{
-         //await fetch('localhost/apiComentario',options)
-         console.log("Paquete:"+paquete.value +"Nombre: "+ nombre.value +"Comentario: "+ comentario.value)
-         console.log("hola")
-      } catch (error) {
-         console.error(error)
-      }
-   }
-
-   sendElemento.addEventListener('click',sendMessage)
-}
+});
 
 
